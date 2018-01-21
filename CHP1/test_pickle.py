5ab0d63ac5ccacc4211093747b3f3e557019fb12
freshield <zxdsw199182@gmail.com>
2018-01-22 00:04:19 +0800

    done pickle

--- CHP1/test_pickle.py
+++ CHP1/test_pickle.py
@@ -1,6 +1,38 @@
 import pickle
 import test_PCA as pca
+import numpy as np
 
 V,S,immean = pca.test_pca()
 
-f = open()
+def test_dump():
+    f = open('font_pca_modes.pkl', 'wb')
+    pickle.dump(immean, f)
+    pickle.dump(V, f)
+    f.close()
+
+def test_load():
+    f = open('font_pca_modes.pkl', 'rb')
+    immean = pickle.load(f)
+    V = pickle.load(f)
+    f.close()
+    print immean.shape
+
+def test_with_dump():
+    with open('font_pca_modes.pkl', 'wb') as f:
+        pickle.dump(immean, f)
+        pickle.dump(V,f)
+
+def test_with_load():
+    with open('font_pca_modes.pkl', 'rb') as f:
+        immean = pickle.load(f)
+        V = pickle.load(f)
+        print immean.shape
+
+def test_numpy_save():
+    np.savetxt('test.txt', immean, '%i')
+
+def test_numpy_load():
+    x = np.loadtxt('test.txt')
+    print x.shape
+
+test_numpy_load()
