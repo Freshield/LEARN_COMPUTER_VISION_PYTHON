4affb0b32d786a4c556a7a0ca1a56cc02445f707
freshield <zxdsw199182@gmail.com>
2018-01-23 23:52:35 +0800

    done pca

---
+++ CHP1/test_PCA.py
@@ -0,0 +1,48 @@
+from PIL import Image
+from numpy import *
+from pylab import *
+from imtools import *
+
+
+def pca(X):
+    num_data, dim = X.shape
+
+    mean_X = X.mean(axis=0)
+    X = X - mean_X
+
+    if dim > num_data:
+        M = dot(X, X.T)
+        e, EV = linalg.eigh(M)
+        tmp = dot(X.T, EV)
+        V = tmp[::-1]
+        S = sqrt(e)[::-1]
+        for i in range(V.shape[1]):
+            V[:,i] /= S
+
+    else:
+
+        U,S,V = linalg.svd(X)
+        V = V[:num_data]
+
+    return V,S,mean_X
+
+data_path = '/media/freshield/COASAIR/LEARN_COMPUTER_VISION_PYTHON/example/data/a_thumbs/'
+imlist = get_imlist(data_path)
+
+im = array(Image.open(imlist[0]))
+m,n = im.shape[0:2]
+imnbr = len(imlist)
+
+immatrix = array([array(Image.open(im)).flatten() for im in imlist], 'f')
+
+V,S,immean = pca(immatrix)
+
+figure()
+gray()
+subplot(2,4,1)
+imshow(immean.reshape(m,n))
+for i in range(7):
+    subplot(2,4,i+2)
+    imshow(V[i].reshape(m,n))
+
+show()
