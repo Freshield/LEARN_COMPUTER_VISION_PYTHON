6982ddb3786ed3956109dc35c9080e1a529f5f2f
freshield <zxdsw199182@gmail.com>
2018-01-30 19:48:37 +0800

    done matplotlib tutorial

--- CHP1/test_matplotlib.py
+++ CHP1/test_matplotlib.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from pylab import *
 import numpy as np
+from matplotlib.ticker import NullFormatter  # useful for `logit` scale
 
 data_path = '../example/data/'
 
@@ -10,19 +11,50 @@ def test0():
     # Fixing random state for reproducibility
     np.random.seed(19680801)
 
-    mu, sigma = 100, 15
-    x = mu + sigma * np.random.randn(10000)
+    # make up some data in the interval ]0, 1[
+    y = np.random.normal(loc=0.5, scale=0.4, size=1000)
+    y = y[(y > 0) & (y < 1)]
+    y.sort()
+    x = np.arange(len(y))
 
-    # the histogram of the data
-    n, bins, patches = plt.hist(x, 50, normed=1, facecolor='g', alpha=0.75)
+    # plot with various axes scales
+    plt.figure(1)
 
-    plt.xlabel('Smarts')
-    plt.ylabel('Probability')
-    plt.title('Histogram of IQ')
-    plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
-    plt.axis([40, 160, 0, 0.03])
+    # linear
+    plt.subplot(221)
+    plt.plot(x, y)
+    plt.yscale('linear')
+    plt.title('linear')
     plt.grid(True)
-    plt.title(r'$\sigma_i=15$')
+
+    # log
+    plt.subplot(222)
+    plt.plot(x, y)
+    plt.yscale('log')
+    plt.title('log')
+    plt.grid(True)
+
+    # symmetric log
+    plt.subplot(223)
+    plt.plot(x, y - y.mean())
+    plt.yscale('symlog', linthreshy=0.01)
+    plt.title('symlog')
+    plt.grid(True)
+
+    # logit
+    plt.subplot(224)
+    plt.plot(x, y)
+    plt.yscale('logit')
+    plt.title('logit')
+    plt.grid(True)
+    # Format the minor tick labels of the y-axis into empty strings with
+    # `NullFormatter`, to avoid cumbering the axis with too many labels.
+    plt.gca().yaxis.set_minor_formatter(NullFormatter())
+    # Adjust the subplot layout, because the logit one may take more space
+    # than usual, due to y-tick labels like "1 - 10^{-3}"
+    plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
+                        wspace=0.35)
+
     plt.show()
 
 
