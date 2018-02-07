3b46cb5bb86c229f46d3a6a84cdf4d78ddff744d
freshield <zxdsw199182@gmail.com>
2018-02-07 14:57:10 +0800

    test save

--- CHP1/test_PIL.py
+++ CHP1/test_PIL.py
@@ -1,2 +1,24 @@
 from PIL import Image
+from pylab import *
+import os
 
+data_path = "../example/data/"
+
+def test1():
+    pil_im = Image.open(data_path + 'empire.jpg').convert('L')
+
+    im = array(pil_im)
+
+    imshow(im)
+
+    show()
+
+def test2():
+    outfile = 'test.jpg'
+    infile = Image.open(data_path + 'empire.jpg').convert('L')
+    try:
+        infile.save(outfile)
+    except IOError:
+        print'cannot convert', infile
+
+test2()
